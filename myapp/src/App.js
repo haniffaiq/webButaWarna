07e853f6755e082c9aa5e5e1368cde447105afd2
hanifffaiq <haniffaiqx3@gmail.com>
2022-07-14 12:41:12 +0700

    first commit

---
+++ myapp/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TableData from './components/TableData';
+
+function App() {
+  return (
+    <div className="App">
+      <TableData/>
+    </div>
+  );
+}
+
+export default App;
---
+++ myapp/src/components/TableData.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Table, Card } from 'react-bootstrap';
+
+const TableData = () => {
+    return (
+        <div>
+            <Card>
+                <Card.Body >
+                    <Table striped bordered hover size="m">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Username</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td colSpan={2}>Larry the Bird</td>
+                                <td>@twitter</td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                </Card.Body>
+            </Card>
+
+        </div>
+    );
+};
+
+export default TableData;
